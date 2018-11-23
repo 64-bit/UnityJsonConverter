700766c6ca08ee838cedb5e32b22c26ec7f50fff
Jonathan Linsner <jonathan.linsner@immersv.co>
2018-11-22 16:08:32 -0800

    Inital code commit

---
+++ Editor/UnityJsonConverterTests.cs
@@ -0,0 +1,258 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace FullbyteGames.Utilities
+{
+    /// <summary>
+    /// Test cases for the UnityJsonConverter class
+    /// </summary>
+    public class UnityJsonConverterTests
+    {
+        private const float FLOAT_COMPARISON_DELTA = 0.00001f;
+
+        [Test]
+        public void Vector2Test()
+        {
+            Vector2 testValue = Random.insideUnitCircle;
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(testValue[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Vector2>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void Vector3Test()
+        {
+            Vector3 testValue = Random.insideUnitSphere;
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(testValue[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Vector3>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void Vector4Test()
+        {
+            Vector4 testValue = Random.insideUnitSphere;
+            testValue.w = Random.value;
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(testValue[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Vector4>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void QuaternionTest()
+        {
+            Quaternion testValue = Random.rotation;
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 4; i++)
+            {
+                Assert.AreEqual(testValue[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Quaternion>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void QuaternionWFirstTest()
+        {
+            //Create special serializer and configure the converter to put w values first
+            var serializer = GetConfiguredSerializer(out var jsonConverter);
+            jsonConverter.QuaternionWComponentFirst = true;
+
+            Quaternion testValue = Random.rotation;
+            //Convert to string
+            var asString = WriteToString(testValue, serializer);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+
+            //Test for w first
+            Assert.AreEqual(testValue[3], asJsonArray[0].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+
+            //Check that xyz are in positions 1,2 and 3
+            for (int i = 0; i < 3; i++)
+            {                                 //offset json array as w is in the first index
+                Assert.AreEqual(testValue[i], asJsonArray[i+1].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Quaternion>(asString, serializer);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void Matrix4x4Test()
+        {
+            Matrix4x4 testValue = new Matrix4x4();
+            for (int i = 0; i < 16; i++)
+            {
+                testValue[i] = Random.value;
+            }
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 16; i++)
+            {
+                Assert.AreEqual(testValue[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Matrix4x4>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue == deserializedvalue);
+        }
+
+        [Test]
+        public void RayTest()
+        {
+            Ray testValue = new Ray
+            {
+                origin = Random.insideUnitSphere,
+                direction = Random.onUnitSphere
+            };
+
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(testValue.origin[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {                                //Offset for direction
+                Assert.AreEqual(testValue.direction[i], asJsonArray[i+3].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Ray>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue.origin == deserializedvalue.origin);
+            Assert.IsTrue(testValue.direction == deserializedvalue.direction);
+        }
+
+        [Test]
+        public void PlaneTest()
+        {
+            Plane testValue = new Plane
+            {
+                normal = Random.onUnitSphere,
+                distance = Random.value
+            };
+
+            //Convert to string
+            var asString = WriteToString(testValue);
+            //Test the array values
+            var asJsonArray = JArray.Parse(asString);
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(testValue.normal[i], asJsonArray[i].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+            }
+            //Offset for distance
+            Assert.AreEqual(testValue.distance, asJsonArray[3].ToObject<float>(), FLOAT_COMPARISON_DELTA);
+
+            //deserialize
+            var deserializedvalue = ReadFromString<Plane>(asString);
+
+            //Test the resultant object, Unity overloads the == operator to do appropriate float comparisons for vectors
+            Assert.IsTrue(testValue.normal == deserializedvalue.normal);
+            Assert.AreEqual(testValue.distance , deserializedvalue.distance, FLOAT_COMPARISON_DELTA);
+        }
+
+
+        private string WriteToString(object obj, JsonSerializer serializer = null)
+        {
+            if (serializer == null)
+            {
+                serializer = GetConfiguredSerializer();
+            }
+            using (var sw = new StringWriter())
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, obj);
+                }
+               return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        private T ReadFromString<T>(string asString, JsonSerializer serializer = null)
+        {
+            if(serializer == null)
+            {
+                serializer = GetConfiguredSerializer();
+            }
+
+            using (var sr = new StringReader(asString))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    return serializer.Deserialize<T>(reader);
+                }
+            }
+        }
+
+        private JsonSerializer GetConfiguredSerializer()
+        {
+            var serializer = new JsonSerializer();
+            var converter = new UnityJsonConverter();
+            serializer.Converters.Add(converter);
+            return serializer;
+        }
+
+        private JsonSerializer GetConfiguredSerializer(out UnityJsonConverter converter)
+        {
+            var serializer = new JsonSerializer();
+            converter = new UnityJsonConverter();
+            serializer.Converters.Add(converter);
+            return serializer;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# UnityJsonConverter
+# Unity Json Converter
+
+This project is a Json Value converter for use with Unity and the Newtonsoft Json.NET library.
+
+By default, this library will not correctly serialize Unity math types (Vector3, Quaternion, ect...) as they have a number of public properties that will be serialized as well. The UnityJsonConverter provides a implementation for correctly serialzing and deserialzing the Unity math types.
+
+Note: This value converter will not work for entire MonoBehaviours, only classes and structs that contain the Unity math types.
+
+## Getting Started
+### Prerequisites
+
+This project requires the Newtonsoft Json.NET library (https://www.newtonsoft.com/json)
+This project requires `.NET 4.x` API compatibility to be enabled in the Unity player settings
+
+### Installing
+
+Copy _```UnityJsonConverter.cs```_ into your Unity project Assets folder.
+Optionally include _```/Editor/UnityJsonConverterTests.cs```_ if you would like to run the unit tests.
+
+### Usage
+Construct a _UnityJustonConverter_ and pass it to the _JsonConverter[]_ argument in _JsonConvert.SerializeObject()_ or _JsonConvert.DeserializeObject()_
+````
+            var vector = new Vector3(0.0f, 1.0f, 2.0f);
+            var unityJsonConverter = new UnityJsonConverter();
+
+            //Returns the string '[0.0,1.0,2.0]'
+            var result = JsonConvert.SerializeObject(vector, unityJsonConverter);
+````
+Construct a _JsonSerializer_ and add the _UnityJsonConverter_ to the _Converters_ collection, then use the class as normal
+````
+var vector = new Vector3(0.0f,1.0f,2.0f);
+
+var serializer = new JsonSerializer();
+var unityJsonConverter = new UnityJsonConverter();
+serializer.Converters.Add(unityJsonConverter);
+
+using (var stringWriter = new StringWriter())
+{
+    using (JsonWriter writer = new JsonTextWriter(stringWriter))
+    {
+        serializer.Serialize(writer, vector);
+    }
+
+    //Returns the string '[0.0,1.0,2.0]'
+    var result = stringWriter.ToString();
+}
+````
+
+### Supported Types
+The following types are currently supported by the converter.
+````
+UnityEngine.Vector2
+UnityEngine.Vector3
+UnityEngine.Vector4
+UnityEngine.Quaternion
+UnityEngine.Matrix4x4
+UnityEngine.Ray
+UnityEngine.Plane
+````
+All supported types are written to a JSON array of numbers
+Vector values are written in xyzw der
+Quaterion values are written in xyzw order by default
+Quaterion values can be written in wxyz order by setting the _UnityJsonConverter.QuaternionWComponentFirst_ field to true
+Matrix values are written in the order the [] accessor reads values
+Ray values are written as ray.origin.xyz ray.direction.xyz
+Plane values are written as plane.normal.xyz plane.distance
+
+## Running the tests
+
+The unit tests for this project are setup to run under the Unity test runner.
+They will show up in the test runner under _FullbyteGames/Utilities/UnityJsonConverterTests_
+
+
+## Authors
+
+* **Jonathan Linsner** - *Initial work* - [64-bit on github](https://github.com/64-bit/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Thanks to Newtonsoft for the great JSON parsing library
